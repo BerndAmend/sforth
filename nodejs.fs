d66349cc4d5f985a8cf902a2082ed6390345dd47
Bernd Amend <bernd.amend@gmail.com>
2013-08-26 22:06:09 +0200

    fix typo

--- nodejs.fs
+++ nodejs.fs
@@ -117,8 +117,6 @@ begin-until-test
 \ : begin-while-repeat-test  ( -- )  7 BEGIN 1+ dup 12 < WHILE dup cr . REPEAT 5 spaces . ;
 \ begin-while-repeat-test
 
-: begin-again-test-1   ( -- 7 )  7 BEGIN 1+ dup 12 = IF " erreicht " . EXIT THEN AGAIN " wird nie erreicht " . ;
-: begin-again-test-2   ( -- 12 )  7 BEGIN 1+ dup 12 = IF " erreicht " . EXIT THEN AGAIN " wird nie erreicht " . ;
+: begin-again-test   ( -- 12 )  7 BEGIN 1+ dup 12 = IF " erreicht " . EXIT THEN AGAIN " wird nie erreicht " . ;
 
-begin-again-test-1
-begin-again-test-2
+begin-again-test
