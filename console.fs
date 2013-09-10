b0cd8d16990b93e57082e29bbcaadd1e0a5a2fcf
Bernd Amend <bernd.amend@gmail.com>
2013-09-10 08:50:21 +0200

    fix output of 0

--- console.fs
+++ console.fs
@@ -1,6 +1,6 @@
 
 \ : print { x -- }  :[ process.stdout.write(x.toString()); ]:d ;
-: print { x -- }  :[ if(x || x === "") process.stdout.write(x.toString()); else console.log(x); ]:d ;
+: print { x -- }  :[ if(x || x === "" || x == 0 ) process.stdout.write(x.toString()); else console.log(x); ]:d ;
 \ : print { x -- }  :[ if(x) console.log(x.toString()); else console.log(x); ]:d ;
 : hex2str { x1 -- x2 } 16 x1.toString ;
 : hexPrint hex2str . ;
