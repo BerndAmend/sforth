c6d6ef21447e72459068216e6cb4bd940e7be833
Bernd Amend <bernd.amend@gmail.com>
2014-03-04 22:31:16 +0100

    update string test

--- tests/strings.fs
+++ tests/strings.fs
@@ -1,9 +1,8 @@
-clearStack
+clearstack
 
-" test string " { str }
-str " test string 2 " = not " strings don't match " assert
-str " test string " = " two identical strings are equal " assert
+»test string« { str }
+str »test string 2« = not »strings don't match« assert
+str »test string« = »two identical strings are equal« assert
 
 \ print a string
-." Teststring "
-" Teststring " .
+"Teststring .
