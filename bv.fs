8941a1ab1770cf4c0487a112304126cd694b7574
Bernd Amend <bernd.amend@gmail.com>
2014-04-21 04:54:33 +0200

    split html/sforth.html into html/editor.html and html/image-processing.html

--- bv.fs
+++ bv.fs
@@ -1,11 +1,34 @@
- : processImage { image }
-	' image.data { dst }
+:js processImage { image ctx }
+	' image.data { src }
 
-	0 ' dst.length 4 do i
-		m+ i 2 { j }
-		m@ dst i { r }
-		m@ dst j { b }
-		m! dst i b
-		m! dst j r
+	:[ ctx.createImageData(image) ]: { output }
+	' output.data { dst }
+
+	0 ' image.height 1 do y
+		m* y image.width { y-pos }
+		m* y-pos 4 { y-pos }
+		0 ' image.width 1 do x
+			m* x 4 { i }
+			m+ i y-pos { i }
+
+			m+ i 1 { ig }
+			m+ i 2 { ib }
+			m+ i 3 { ia }
+
+			m@ src i { r }
+			m@ src ig { g }
+			m@ src ib { b }
+			m@ src ia { a }
+			m! dst i b
+			m! dst ig g
+			m! dst ib r
+			m! dst ia a
+		loop
 	loop
+
+	' output 0 0 ctx.putImageData
+	/* 20 20 800 400 ctx.rect
+	"1 to ctx.lineWidth
+	"red to ctx.strokeStyle
+	ctx.stroke */
 ;
