0674f33fd78134fe8763931f1d3c455431fcd193
Bernd Amend <bernd.amend@gmail.com>
2014-04-21 22:02:52 +0200

    browser.fs: add new function create-worker

--- browser.fs
+++ browser.fs
@@ -1,1 +1,7 @@
 : console-log { msg } :[ console.log(msg) ]; ;
+
+: create-worker { id -- worker }
+	:[ new Blob([document.querySelector(id).textContent]) ]: { blob }
+	:[ new Worker(window.URL.createObjectURL(blob)) ]: { worker }
+	worker
+;
