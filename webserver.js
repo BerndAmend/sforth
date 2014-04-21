2ef9df3e822919dcd13267dd3eddcefcb064b41c
Bernd Amend <bernd.amend@gmail.com>
2014-04-21 03:00:42 +0200

    update webserver

--- webserver.js
+++ webserver.js
@@ -1,37 +1,50 @@
-var http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs")
-    port = process.argv[2] || 8888;
+var
+http = require('http'),
+path = require('path'),
+url = require("url"),
+fs = require('fs'),
+
+extensions = {
+    ".html" : "text/html",
+    ".css" : "text/css",
+    ".js" : "application/javascript",
+	".json" : "application/json",
+    ".png" : "image/png",
+    ".gif" : "image/gif",
+    ".jpg" : "image/jpeg"
+};
 
 http.createServer(function(request, response) {
 
-  var uri = url.parse(request.url).pathname
-    , filename = path.join(process.cwd(), uri);
-  
-  path.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
-      response.end();
-      return;
-    }
+	var uri = url.parse(request.url).pathname
+		, filename = path.join(process.cwd(), uri)
+		, ext = path.extname(uri);
 
-    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+	fs.exists(filename, function(exists) {
+		if(!exists) {
+			response.writeHead(404, {"Content-Type": "text/plain"});
+			response.write("404 Not Found\n");
+			response.end();
+			return;
+		}
 
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
+		if (fs.statSync(filename).isDirectory())
+			filename += '/index.html';
 
-      response.writeHead(200);
-      response.write(file, "binary");
-      response.end();
-    });
-  });
-}).listen(parseInt(port, 10));
+		fs.readFile(filename, "binary", function(err, file) {
+			if(err) {
+				response.writeHead(500, {"Content-Type": "text/plain"});
+				response.write(err + "\n");
+				response.end();
+				return;
+			}
 
-console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+			var content_type = "text/plain";
+			if(extensions[ext])
+				content_type = extensions[ext];
+			response.writeHead(200, {"Content-Type": content_type});
+			response.write(file, "binary");
+			response.end();
+		});
+	});
+}).listen(8080);
