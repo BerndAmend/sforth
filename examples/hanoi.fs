e90c70c39d0cecafb931ac190b8cbf803af179d1
Bernd Amend <bernd.amend@gmail.com>
2014-04-26 22:20:12 +0200

    simplify examples/hanoi.fs

--- examples/hanoi.fs
+++ examples/hanoi.fs
@@ -1,19 +1,15 @@
-: left   "left . ;
-: right  "right . ;
-: middle "middle . ;
- 
 : move-disk ( v t f n -- v t f )
   dup 0= if drop exit then
   1-       >r
   rot swap r@ ( t v f n-1 ) move-disk
   rot swap
-  2dup cr »Move disk from « . execute » to « . execute
+  2dup "\n »Move disk from « + swap + » to « + swap + .
   swap rot r> ( f t v n-1 ) move-disk
   swap rot ;
 
 : hanoi ( n -- )
   1 Math.max >r
-  ' right ' middle ' left r> move-disk
+  "right "middle "left r> move-disk
   drop drop drop ;
 
 3 hanoi
