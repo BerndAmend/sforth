1a9ff437ec7d9044d56b0ea3b606b4d79701300a
Bernd Amend <bernd.amend@gmail.com>
2023-10-08 19:31:56 +0200

    add serial port example

--- examples/denoSerialTest.fs
+++ examples/denoSerialTest.fs
@@ -1,9 +1,43 @@
 include "forth.fs"
 include »console.fs«
+"https://raw.githubusercontent.com/BerndAmend/deno_serial/master/main.ts" import await { serial }
 
-"https://deno.land/x/deno_serial@0.1.0/main.ts" import await { denoSerial }
+:[ new serial.SerialPort("/dev/ttyACM0") ]: { port }
 
-"/dev/ttyUSB0" :[ { baudrate: denoSerial.Baudrate.B115200, minimum_number_of_chars_read: 0, timeout_in_deciseconds: 10} ]: denoSerial.open(2) await { serial }
-"Fisch\n" serial.write_string(1) await;
-serial.read_string() await .
-serial.read_string() await .
+:[ { baudRate: 115200 } ]: port.open(1) await;
+
+:[ const lineReader = port.readable
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(new TransformStream(new serial.LineBreakTransformer()));
+];
+
+// write stuff every 200msec
+:async writeStuff {}
+    new TextEncoder const encoder
+    port.writable.getWriter(0) const writable
+    try
+        0 1000 1 do i
+          `Test message ${i}\n` encoder.encode(1) writable.write(1) await;
+          200 sleep await;
+        loop
+		catch e
+			`\n${SForthSystem.demangle(e.stack)}\n` .
+		endtry
+;
+
+:async readStuff {}
+lineReader.getReader(0) { reader }
+    0 begin
+      reader.read(0) await { line }
+      line.done if break endif
+      line.value "\n" + .
+    again
+;
+
+// both functions run in the background
+writeStuff
+readStuff
+
+  // exit after 5 seconds
+5000 sleep(1) await;
+port.close(0);
