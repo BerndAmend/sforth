f4b533e096bb95156ea0a66cb163a0251586f42e
Bernd Amend <bernd.amend@gmail.com>
2023-12-17 03:06:34 +0100

    tests/localevar.fs: simplify test

--- tests/localevar.fs
+++ tests/localevar.fs
@@ -1,7 +1,6 @@
-:noname {}
-  1 2 { x1 y1 }
-  3 4 { x2 y2 }
-  y1 y2 = drop
-  :[ x1 == 1 ]: "x1 should be 1" assert
-  :[ x2 == 3 ]: "x3 should be 3" assert
-; execute
+  1 2 { x1 y1 }
+  3 4 { x2 y2 }
+  y1 y2 = drop
+  // If this happens local variables are not correctly handled
+  :[ x1 == 1 ]: `x1 should be 1, but is ${x1}` assert
+  :[ x2 == 3 ]: `x2 should be 3, but is ${x2}` assert
