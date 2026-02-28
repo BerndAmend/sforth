9380f23e62d32a521e90a4bf14fdc7e4fca60b0b
Bernd Amend <typescript@berndamend.de>
2026-02-28 22:33:50 +0100

    repl: add support for editing lines

--- repl.fs
+++ repl.fs
@@ -45,6 +45,7 @@ Type `bye' to exit\n« .
 "" { entered }
 
 0 { cmd_last_pos }
+0 { cmd_cursor_pos }
 new SForthStack { cmd_history }
 
 try
@@ -62,19 +63,30 @@ null to forthconsole.onKey
 		null to forthconsole.onKey
 	elseif ' forthconsole.onKey null <> if
 		key forthconsole.onKey await;
-	elseif key "\u007f" === if
+	elseif key "\u007f" === key "\b" === || if
 		\ Backspace was pressed
-		entered.length 0> if
-			0 entered.length 1- entered.slice(2) to entered
-		clearcurrentline
+		cmd_cursor_pos 0> if
+			0 cmd_cursor_pos 1- entered.slice(2)
+			cmd_cursor_pos entered.length entered.slice(2) + to entered
+			cmd_cursor_pos 1- to cmd_cursor_pos
+			clearcurrentline
 			entered.length 0> if entered . endif
+			entered.length cmd_cursor_pos - { diff }
+			diff 0> if
+				»\u001B[${diff}D« type
+			endif
 		endif
 	elseif key "\r" === key "\n" === || if
+		entered.length cmd_cursor_pos - { diff }
+		diff 0> if
+			»\u001B[${diff}C« type
+		endif
 		" " type
 		entered "" !== if
 			entered cmd_history.push
 		endif
 		0 to cmd_last_pos
+		0 to cmd_cursor_pos
 
 		try
 			entered
@@ -103,6 +115,7 @@ null to forthconsole.onKey
 					endif
 					cmd_history.size 0> if
 						clearcurrentline cmd_last_pos cmd_history.get to entered
+						entered.length to cmd_cursor_pos
 					endif
 					entered .
 				endof
@@ -113,6 +126,7 @@ null to forthconsole.onKey
 					endif
 					cmd_history.size 0> if
 						clearcurrentline cmd_last_pos cmd_history.get to entered
+						entered.length to cmd_cursor_pos
 					endif
 					entered .
 					cmd_last_pos 0= if
@@ -123,9 +137,47 @@ null to forthconsole.onKey
 				endof
 
 				of 68 \ left
+					cmd_cursor_pos 0> if
+						cmd_cursor_pos 1- to cmd_cursor_pos
+						"\u001B[1D" type
+					endif
 				endof
 
 				of 67 \ right
+					cmd_cursor_pos entered.length < if
+						cmd_cursor_pos 1+ to cmd_cursor_pos
+						"\u001B[1C" type
+					endif
+				endof
+
+				of 72 \ home (Pos1)
+					cmd_cursor_pos 0> if
+						»\u001B[${cmd_cursor_pos}D« type
+						0 to cmd_cursor_pos
+					endif
+				endof
+
+				of 70 \ end (Ende)
+					cmd_cursor_pos entered.length < if
+						entered.length cmd_cursor_pos - { diff }
+						»\u001B[${diff}C« type
+						entered.length to cmd_cursor_pos
+					endif
+				endof
+
+				of 51 \ delete
+					3 key.charCodeAt 126 = if
+						cmd_cursor_pos entered.length < if
+							0 cmd_cursor_pos entered.slice(2)
+							cmd_cursor_pos 1+ entered.length entered.slice(2) + to entered
+							clearcurrentline
+							entered.length 0> if entered . endif
+							entered.length cmd_cursor_pos - { diff }
+							diff 0> if
+								»\u001B[${diff}D« type
+							endif
+						endif
+					endif
 				endof
 
 				default
@@ -134,8 +186,21 @@ null to forthconsole.onKey
 	else
 		// If a user copies a string with new lines into the repl we are
 		// only called once.
-		"\r" "\n" key.replaceAll type
-		entered key + to entered
+		"\r" "\n" key.replaceAll { clean_key }
+		cmd_cursor_pos entered.length === if
+			clean_key type
+			entered clean_key + to entered
+			cmd_cursor_pos clean_key.length + to cmd_cursor_pos
+		else
+			0 cmd_cursor_pos entered.slice(2) clean_key + cmd_cursor_pos entered.length entered.slice(2) + to entered
+			cmd_cursor_pos clean_key.length + to cmd_cursor_pos
+			clearcurrentline
+			entered .
+			entered.length cmd_cursor_pos - { diff }
+			diff 0> if
+				»\u001B[${diff}D« type
+			endif
+		endif
 	endif
 ;
 
